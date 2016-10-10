d50b30a6bc01af4b44f7c961955ab8b49084d5a4
Jacob Arriola <jacob@zeek.com>
2016-10-09 20:19:27 -0700

    Add corss domain transfer

--- themes/estimator/functions.php
+++ themes/estimator/functions.php
@@ -1,9 +1,28 @@
 <?php
 namespace Estimator;
 
-add_filter('http_origin', function() {
-	return $_SERVER[ 'HTTP_ORIGIN' ];
+//add_filter('http_origin', function() {
+//	return $_SERVER[ 'HTTP_ORIGIN' ];
+//});
+
+add_filter( 'json_pre_serve_request', function() {
+	header( 'Access-Control-Allow-Origin', '*' );
 });
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+	header("Access-Control-Allow-Origin: *");
+	header('Access-Control-Allow-Credentials: true');
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+}
+
+// Accept Cross Domain Transfer.
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+		header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+	exit(0);
+}
 
 add_action( 'init', function() {
 	$labels = array(
